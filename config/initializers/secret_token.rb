861425a0adabda1721a9426d1d9b8519e6f46920
weidenfreak <meike.wiemann@gmx.de>
2012-07-29 14:03:28 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backpacker::Application.config.secret_token = '4a45e847e9a0fa92c915c22042973a0763b78635d577c62448d1afe1bfc605a1ebef3d52b780dc10a853949bdde2b26bedd1c97196a2606e7edd199f4875f106'

c3dd677d234e2fcd949bb7df2da7dc8f5d93b714
weidenfreak <meike.wiemann@gmx.de>
2012-08-20 14:02:42 +0200

    add name to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -6,6 +7,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 end

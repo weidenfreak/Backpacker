6765cae023c6b17932bb97cbfa932643bd09b8f3
weidenfreak <meike.wiemann@gmx.de>
2012-08-21 21:12:33 +0200

    add quick and dirty imprint

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 # -*- encoding : utf-8 -*-
 class WelcomeController < ApplicationController
   def index
+  end
 
+  def imprint
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Backpacker::Application.routes.draw do
   devise_for :users
   resources :users, :only => [:show]
 
+  match '/imprint', :to => 'welcome#imprint'
+
   root :to => 'welcome#index'
 end

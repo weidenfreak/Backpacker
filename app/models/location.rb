56cfba2464189385082147b9a5216a71e5fcb60d
weidenfreak <meike.wiemann@gmx.de>
2012-10-27 15:09:35 +0200

    remove location model gmap4rails doesn't allow the longitude and latitude in another model except the user

--- app/models/location.rb
+++
@@ -1,7 +0,0 @@
-# -*- encoding : utf-8 -*-
-class Location < ActiveRecord::Base
-  attr_accessible :latitude, :longitude
-
-  belongs_to :user
-
-end
--- spec/factories/locations.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :location do
-    latitude 1.5
-    longitude 1.5
-  end
-end

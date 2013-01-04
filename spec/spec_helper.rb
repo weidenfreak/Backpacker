b5690ec44ba7dcf54e3bc42957f2682d09c8f073
weidenfreak <meike.wiemann@gmx.de>
2013-01-04 14:37:14 +1300

    add helper for controller specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 RSpec.configure do |config|
   config.mock_with :mocha
   config.include Devise::TestHelpers, :type => :controller
+  config.include ControllerMacros, :type => :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
---
+++ spec/support/controller_macros.rb
@@ -0,0 +1,7 @@
+# -*- encoding : utf-8 -*-
+module ControllerMacros
+  def login_user
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in FactoryGirl.create(:user)
+  end
+end

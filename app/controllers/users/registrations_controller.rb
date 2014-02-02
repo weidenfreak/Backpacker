6e2793b2f8d2a8daa2054b82f23172b019886898
weidenfreak <meike.wiemann@gmx.de>
2014-02-02 14:12:57 +0100

    fixes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,9 +6,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     if @user.update_without_password(params[:user])
       set_flash_message :notice, :updated
+      redirect_to root_url
+    else
+      render "devise/registrations/edit"
     end
 
-    redirect_to edit_user_registration_path
   end
 
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 class WelcomeController < ApplicationController
   def index
-    @users = User.all
+    @users = User.all.select{ |user| user.address.present? }
     @json = Gmaps4rails.build_markers(@users) do |user, marker|
       marker.lat user.latitude
       marker.lng user.longitude
